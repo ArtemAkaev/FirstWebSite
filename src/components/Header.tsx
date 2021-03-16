7643a524aeece5e19ad9595c8e9e1cc985188c14
ArtemAkaev <akaevfamily@gmail.com>
2021-03-16 23:45:22 +0300

    Change README, and add screenshot

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className={"header"}>
+        <img src={"https://logodix.com/logo/3470.png"}/>
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className={"content"}>
+        <div>
+            <img src={"https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?cs=srgb&dl=scenic-view-of-beach-248797.jpg&fm=jpg"}/>
+        </div>
+        <div>
+            <img src={"https://i.pinimg.com/originals/b1/e7/08/b1e70852622b7b4fc5a4eecb65931eda.jpg"}/>
+        </div>
+        <div>
+            My posts
+            <div>
+                New post
+            </div>
+            <div>
+                Post1
+            </div>
+            <div>
+                Post2
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile;
